20047006549105afe7cddc775ab31a05d3124ef5
Антон Матушкин <matushkin-2017@mail.ru>
2024-11-27 18:05:38 +0500

    chore: initial commit

---
+++ src/main/java/com/dlvb/asyncbankapp/AsyncBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.dlvb.asyncbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncBankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncBankAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dlvb/asyncbankapp/AsyncBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlvb.asyncbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
